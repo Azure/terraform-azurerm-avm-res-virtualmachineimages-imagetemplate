13d29026551cf9f3c55831603cd2654ea99b17d0
Travis Hankins <travis@MacBook-Pro.local>
2025-10-18 09:22:33 -0600

    chore: avm pre-commit

--- examples/windows-image/main.tf
+++ examples/windows-image/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "2.4.0"
+      version = "1.12.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
