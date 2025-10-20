0bd05d2771f744b26267240d0b9896574742715e
Travis Hankins <travishankins@users.noreply.github.com>
2025-10-20 08:30:12 -0600

    fix: use azapi_resource identity block and restrict to UserAssigned only

--- locals.tf
+++ locals.tf
@@ -1,20 +1,7 @@
 locals {
-  image_template_body = merge(
-    {
-      properties = local.image_template_properties
-    },
-    local.image_template_identity == null ? {} : {
-      identity = local.image_template_identity
-    }
-  )
-  image_template_identity = var.identity == null ? null : merge(
-    {
-      type = var.identity.type
-    },
-    length(local.user_assigned_identity_map) == 0 ? {} : {
-      userAssignedIdentities = local.user_assigned_identity_map
-    }
-  )
+  image_template_body = {
+    properties = local.image_template_properties
+  }
   image_template_properties = merge(
     {
       source     = var.source_image
@@ -32,9 +19,6 @@ locals {
     }
   )
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  user_assigned_identity_map = var.identity == null || var.identity.user_assigned_resource_ids == null ? {} : {
-    for id in var.identity.user_assigned_resource_ids : id => {}
-  }
   vm_profile = merge(
     var.vm_profile_overrides,
     var.vm_size == null ? {} : {
